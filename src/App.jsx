099057c7017de8a457ee26697490a0bd9124c958
Md. Mohiul Islam Miraz <mohiulislam23@gmail.com>
2025-03-01 17:41:47 +0600

    Designed Profiles Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Banner from './Components/Banner';
 import About from './Components/About';
 import Skills from './Components/Skills';
 import Contact from './Components/Contact';
+import Profiles from './Components/Profiles';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Banner/>
       <About/>
       <Skills/>
+      <Profiles/>
       <Contact/>
     </div>
   );
---
+++ src/Components/Profiles.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect } from 'react'
+import { Link } from "react-scroll"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import CodeforcesLogo from '../asset/codeforces.png'
+import LeetcodeLogo from '../asset/leetcode.png'
+import IeeeLogo from '../asset/ieee.png'
+
+const Skills = () => {
+    useEffect(() => {
+        AOS.init();
+    }, [])
+
+    return (
+        <div name='skills' className='mx-20 font-sans'>
+            <div className='flex flex-col justify-center items-center max-w-[1080px] mx-auto'>
+                <div className='py-20'>
+                    <p className='text-4xl font-bold inline border-b-8 border-[#C23B22]'>Profiles</p>
+                </div>
+
+                <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center mx-auto'>
+                    <div className='shadow-2xl shadow-[#040c16] hover:scale-110 duration-300 rounded-md cursor-pointer'>
+                        <a href="https://codeforces.com/profile/mohiul_islam">
+                            <div data-aos='zoom-in-up' data-aos-duration='1500'>
+                                <img src={CodeforcesLogo} className='w-25 mx-auto' alt="html icon" />
+                                <p className='my-6'>Codeforces <br /> (450+ Solve)</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div className='shadow-2xl shadow-[#040c16] hover:scale-110 duration-300 rounded-md'>
+                        <a href="https://leetcode.com/u/mohiul_islam/">
+                            <div data-aos='zoom-in-up' data-aos-duration='1500'>
+                                <img src={LeetcodeLogo} className='w-21 mx-auto' alt="css icon" />
+                                <p className='my-6'>Leetcode <br /> (100+ Solve)</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div className='shadow-2xl shadow-[#040c16] hover:scale-110 duration-300 rounded-md'>
+                        <a href="https://ieee-collabratec.ieee.org/app/p/mohiul-islam-miraz">
+                            <div data-aos='zoom-in-up' data-aos-duration='1500'>
+                                <img src={IeeeLogo} className='w-45 mx-auto' alt="css icon" />
+                                <p className='my-6'>IEEE CS<br />(Public Relations Executive)</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Skills
